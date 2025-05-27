17ab1ff9d3d67640bfeff4f5a8df9679f59f29ec
Rahul K Murali <rahulkmurali@gmail.com>
2025-05-27 23:30:03 +0530

    login

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from './UserSlice';
+import authReducer from './AuthSlice';
 
 export const store = configureStore({
     reducer: {
         users: userReducer,
+        auth: authReducer
     }
 })
